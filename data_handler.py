78324263bdfa00967353c102cd84ecce12b096c5
ambiwalencja <helena.a.sokolowska@gmail.com>
2022-03-21 00:41:49 +0100

    further work on filtering data acquired fron portal

--- data_handler.py
+++ data_handler.py
@@ -5,12 +5,12 @@ import json
 
 
 def get_data():
-    with open("token_2.txt", "r") as file:
+    with open("token.txt", "r") as file:
         access_token = file.readline()
     request_url = 'https://api.portal.futurecollars.com/api/query/FutureCollars.Core.Contracts.Mentor.MentorTasks.Queries.MentorTasksHistory'
     response = requests.post(
         request_url,
-        json={ # jak tutaj było "data" to nie działałom (był błąd 400 bad request), po zmianie na json zadziałało!
+        json={ # jak tutaj było "data" to nie działało (był błąd 400 bad request), po zmianie na json zadziałało!
             'DateCreatedFrom': "2022-02-19T00:00:00.000Z",
             'DateCreatedTo': "2022-03-19T23:59:59.999Z"
         },
@@ -24,15 +24,41 @@ def get_data():
 
 def save_data_to_file(data):
     with open('data.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+        json.dump(data, f, ensure_ascii=False, indent=4)  # json.dump puts the data into file
+    return True
+
 
 def read_data_from_file(filename):
+    with open(filename, encoding="utf8") as json_file:  # musialam dodać encoding, bo inaczej nie chciało czytać
+        data = json.load(json_file)
+    return data
 
 
+def create_mentor_list(data): # funkcja do opracowania
+    mentor_list = []
+    for task in data:
+        if task['MentorName'] not in mentor_list:
+            mentor_list.append(task['MentorName'])
 
-# def filter_data(mentor_name, response):
+
+def filter_data(mentor_name, data):
+    filtered_data = []
+    for task in data:
+        if task['MentorName'] == mentor_name:
+            filtered_data.append(task)
+    return filtered_data
 
 
-# print(get_data()[0]["MentorName"])
+# print(get_data()[0]["MentorName"])  # this lets me check how does my data look
+# save_data_to_file(get_data())  # this i run once for writing data to file so i can use it for the further development
 
-# save_data_to_file(get_data())
+# read_data_from_file('data.json')
+# print(read_data_from_file('data.json')[0]["MentorName"])
+
+
+my_data = read_data_from_file('data.json')
+my_filtered_data = filter_data('Arkadiusz Radek', my_data)
+print(len(my_filtered_data))
+
+# for number in my_data:
+#     print(number['MentorName'])
