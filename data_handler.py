9ea4f9154a10c27c990a989d5723f2196e706714
ambiwalencja <helena.a.sokolowska@gmail.com>
2022-03-22 00:13:27 +0100

    added creating mentor list, filtering data and writing filtered data to files

--- data_handler.py
+++ data_handler.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-# and now getting the data from portal.futurecollars
+# getting the data from portal.futurecollars
 
 
 def get_data():
@@ -22,13 +22,19 @@ def get_data():
     return response.json()
 
 
-def save_data_to_file(data):
-    with open('data.json', 'w', encoding='utf-8') as f:
+def save_json_data_to_file(json_filename, data):
+    with open(json_filename, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)  # json.dump puts the data into file
     return True
 
 
-def read_data_from_file(filename):
+def save_data_to_file(filename, data):
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(str(data))
+    return True
+
+
+def read_data_from_json_file(filename):
     with open(filename, encoding="utf8") as json_file:  # musialam dodać encoding, bo inaczej nie chciało czytać
         data = json.load(json_file)
     return data
@@ -39,6 +45,7 @@ def create_mentor_list(data): # funkcja do opracowania
     for task in data:
         if task['MentorName'] not in mentor_list:
             mentor_list.append(task['MentorName'])
+    return mentor_list
 
 
 def filter_data(mentor_name, data):
@@ -49,6 +56,11 @@ def filter_data(mentor_name, data):
     return filtered_data
 
 
+def create_files_for_mentors(mentor_list, data): # to mi na razie tworzy pliki z zapisanymi listami
+    for name in mentor_list:
+        save_data_to_file(f'{name}.txt', filter_data(name, data))
+    return True
+
 # print(get_data()[0]["MentorName"])  # this lets me check how does my data look
 # save_data_to_file(get_data())  # this i run once for writing data to file so i can use it for the further development
 
@@ -56,9 +68,10 @@ def filter_data(mentor_name, data):
 # print(read_data_from_file('data.json')[0]["MentorName"])
 
 
-my_data = read_data_from_file('data.json')
-my_filtered_data = filter_data('Arkadiusz Radek', my_data)
-print(len(my_filtered_data))
-
-# for number in my_data:
-#     print(number['MentorName'])
+my_data = read_data_from_json_file('data.json')
+# my_filtered_data = filter_data('Arkadiusz Radek', my_data)
+# save_data_to_file('Arkadiusz_Radek.txt', my_filtered_data)
+my_mentor_list = create_mentor_list(my_data)
+create_files_for_mentors(my_mentor_list, my_data)
+# print(my_mentor_list)
+# print(len(my_filtered_data))
