267dc618c8ca6bf71f0505b0ae78428ac7cf8b1e
ambiwalencja <helena.a.sokolowska@gmail.com>
2022-04-01 22:56:10 +0200

    added writing data to xlsx files

--- data_handler.py
+++ data_handler.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+import xlsxwriter
 # getting the data from portal.futurecollars
 
 
@@ -28,9 +28,41 @@ def save_json_data_to_file(json_filename, data):
     return True
 
 
+def write_headers_to_file(worksheet, data):
+    # tutaj przekleić fragment z wypełnianiem pierwszego wiersza kluczami
+    row = 0
+    col = 0
+    headers_dict = data[0]
+    for key in headers_dict:
+        if key == "Metadata":
+            for metadata_key in headers_dict[key].keys():
+                worksheet.write(row, col, f'{key}: {metadata_key}')
+                col += 1
+        else:
+            worksheet.write(row, col, key)
+            col += 1
+    return True
+
+
 def save_data_to_file(filename, data):
-    with open(filename, 'w', encoding='utf-8') as f:
-        f.write(str(data))
+    workbook = xlsxwriter.Workbook(f'{filename}.xlsx')
+    worksheet = workbook.add_worksheet()
+    row = 0
+    write_headers_to_file(worksheet, data)
+    for task in data:
+        col = 0
+        row += 1
+        for key in task:
+            if key == "Metadata":
+                for metadata_key in task[key].keys():
+                    worksheet.write(row, col, task[key][metadata_key])
+                    col += 1
+            else:
+                worksheet.write(row, col, task[key])
+                col += 1
+    workbook.close()
+    # with open(filename, 'w', encoding='utf-8') as f:
+    #     f.write(str(data))
     return True
 
 
@@ -56,19 +88,23 @@ def filter_data(mentor_name, data):
     return filtered_data
 
 
-def create_files_for_mentors(mentor_list, data): # to mi na razie tworzy pliki z zapisanymi listami
+def create_files_for_mentors(mentor_list, data):
     for name in mentor_list:
-        save_data_to_file(f'{name}.txt', filter_data(name, data))
+        save_data_to_file(name, filter_data(name, data))
     return True
 
+
+
+
+# ___________________________________________________________________________________________________________
 # print(get_data()[0]["MentorName"])  # this lets me check how does my data look
 # save_data_to_file(get_data())  # this i run once for writing data to file so i can use it for the further development
 
 # read_data_from_file('data.json')
 # print(read_data_from_file('data.json')[0]["MentorName"])
 
-
 my_data = read_data_from_json_file('data.json')
+# save_data_to_file('test', my_data)
 # my_filtered_data = filter_data('Arkadiusz Radek', my_data)
 # save_data_to_file('Arkadiusz_Radek.txt', my_filtered_data)
 my_mentor_list = create_mentor_list(my_data)
